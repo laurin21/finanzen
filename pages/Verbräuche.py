82e6d6fe41f21b78c107d1ef89d026a306f9fdee
laurin21 <74432244+laurin21@users.noreply.github.com>
2022-11-30 11:29:46 +0100

    Update Verbräuche.py

--- pages/Verbräuche.py
+++ pages/Verbräuche.py
@@ -9,10 +9,10 @@ sa = gspread.service_account("service_account.json")
 sh = sa.open("verbraeuche_wg")
 
 strom = sh.worksheet("Strom")
-strom = pd.DataFrame(aus.get_all_records())
+strom = pd.DataFrame(strom.get_all_records())
 
 gas = sh.worksheet("Ausgaben")
-gas = pd.DataFrame(ein.get_all_records())
+gas = pd.DataFrame(strom.get_all_records())
 
 
 st.title("Vebräuche WG")
