5c17c5159c8bff87b028982938b9db553fc99494
laurin21 <74432244+laurin21@users.noreply.github.com>
2022-11-30 11:28:37 +0100

    Create Wochenstunden.py

---
+++ pages/Wochenstunden.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+import numpy as np
+import time
+import gspread
+
+sa = gspread.service_account("service_account.json")
+sh = sa.open("wochenstunden")
+
+df = sh.worksheet("Tabelle1")
+df = pd.DataFrame(aus.get_all_records())
+
+
+
+st.title("Wochenstunden")
+
+st.write(df)
+
+st.line_chart(df)
