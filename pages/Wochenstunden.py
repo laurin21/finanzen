0467a475dd919d3cc51c56a2e2ca7485bff7d9af
laurin21 <74432244+laurin21@users.noreply.github.com>
2022-12-01 17:40:52 +0100

    Update Wochenstunden.py

--- pages/Wochenstunden.py
+++ pages/Wochenstunden.py
@@ -30,6 +30,7 @@ delta = round(average - avg_soll, 3)
 
 st.title("Wochenstunden")
 
+sum_h = df["Stunden].sum()
 
 
 ####
@@ -42,4 +43,4 @@ st.write(f"Delta zum Mindestdurchschnitt: {delta}h")
 
 st.write(f"Durchschnittliche Stundenanzahl pro Woche: {average}h")
 
-
+st.write(f"Stunden insgesamt: {sum_h}h"
